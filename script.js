7d46bc9d02e9c5dbfbe01d8b5aa5fd036d1ff4f2
rohit09010901 <rohitsaik09@gmail.com>
2025-12-15 19:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-modal");
+
+openBtn.onclick = () => {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
+
